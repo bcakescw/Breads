1079565af2fdf1bcddb56cd57f43e26f9824dd1c
Courtney Carroll <bcakescw@gmail.com>
2022-08-03 21:27:28 -0400

    bakers controllers added

--- server.js
+++ server.js
@@ -2,9 +2,11 @@
 const express = require('express')
 
 // CONFIGURATION
-require('dotenv').config()
+const dotenv = require("dotenv");
+dotenv.config();
 const PORT = process.env.PORT
 const app = express()
+
 
 // MIDDLEWARE
 app.set('views', __dirname + '/views')
@@ -14,11 +16,11 @@ app.use(express.static('public'))
 // MIDDLEWARE
 app.use(express.urlencoded({extended: true}))
 
+const mongoose = require('mongoose')
 
-// DEPENDENCIES
-const methodOverride = require('method-override')
-// MIDDLEWARE
-app.use(methodOverride('_method'))
+mongoose.connect(process.env.MONGO_URI, {useNewUrlParser: true, useUnifiedTopology: true}, 
+  () => { console.log('connected to mongo: ', process.env.MONGO_URI) }
+)
 
 
 // ROUTES
@@ -30,7 +32,11 @@ app.get('/', (req, res) => {
 const breadsController = require('./controllers/breads_controllers.js')
   app.use('/breads', breadsController)
   
-  //404 page
+  //bakers
+  const bakersController = require('./controllers/bakers_controllers.js')
+  app.use ('/bakers', bakersController)
+  //404 page\
+  
   app.get('*', (req, res)=>{
     res.send('404')
   })
